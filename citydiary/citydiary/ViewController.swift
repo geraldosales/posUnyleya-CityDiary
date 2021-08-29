bdb02514644165ec96ba2fc4c9cc568a6a162be3
Geraldo Sales <falecom@geraldosales.com.br>
2021-08-29 01:13:39 -0300

    initial project commit

---
+++ citydiary/citydiary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  citydiary
+//
+//  Created by Geraldo O Sales Jr on 29/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
